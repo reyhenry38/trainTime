5145166a406052995fc86d28cc7381582cc6d740
Ejike <reyhenry38@gmail.com>
2018-04-27 23:04:55 -0400

    latest changes

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# trainTime
+# Train-Scheduler
+A train schedule web application that incorporates Firebase to host arrival and departure data. 
+
+<img width="400" alt="screen shot 2018-04-27 at 10 58 29 pm" src="https://user-images.githubusercontent.com/31670361/39391288-de67b5fc-4a6e-11e8-8c2e-214994c15620.png">
+
+Trains can be added to the scheduler. The schedule updates when the page is refreshed.
+
+[Heroku app](https://guarded-beach-16060.herokuapp.com/)
+
+Copywrite [EjikeOnwe](github.com/reyhenry38) (C) 2017
---
+++ assets/javascript/app.js
@@ -0,0 +1,87 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyBfEhonJjJLXHswoUbKYvGDBw8_H0rP_2g",
+    authDomain: "my-train-schedule-app.firebaseapp.com",
+    databaseURL: "https://my-train-schedule-app.firebaseio.com",
+    storageBucket: "my-train-schedule-app.appspot.com",
+    messagingSenderId: "119131371906"
+};
+firebase.initializeApp(config);
+
+var database = firebase.database();
+
+//Variable for current time
+var currentTime = moment(new Date());
+currentTime = moment().format('HH:mm');
+
+//Button for adding Trains
+$("#add-train").on("click", function() {
+
+    // Grab user input from text input field
+    var trainName = $("#train-name-input").val().trim();
+    var trainDestination = $("#train-destination-input").val().trim();
+    var trainTime = $("#train-time-input").val().trim();
+    var trainFrequency = parseInt($("#train-frequency-input").val().trim());
+
+
+    // Creates local "temporary" object for holding train data
+    var newTrain = {
+        name: trainName,
+        destination: trainDestination,
+        time: trainTime,
+        frequency: trainFrequency
+    };
+
+    // Uploads train data to the database
+    database.ref().push(newTrain);
+
+    // Alert
+    alert("Train successfully added");
+
+    // Clears all of the text-boxes
+    $("#train-name-input").val("");
+    $("#train-destination-input").val("");
+    $("#train-time-input").val("");
+    $("#train-frequency-input").val("");
+
+    // Prevents loading a new page
+    return false;
+});
+
+// 3. Creates Firebase event for adding train to the database and a row in the html when a user adds a train
+database.ref().on("child_added", function(childSnapshot, prevChildKey) {
+
+    // Store train data into  variables
+    var trainName = childSnapshot.val().name;
+    var trainDestination = childSnapshot.val().destination;
+    var trainTime = childSnapshot.val().time;
+    var trainFrequency = childSnapshot.val().frequency;
+
+    // Train time converted
+    var trainTimeConverted = moment(childSnapshot.val().time, "HH:mm");
+    console.log(trainTimeConverted);
+    // Difference between the times
+    var diffTime = moment().diff(moment(trainTimeConverted), "minutes");
+    console.log(diffTime);
+    // Time apart (remainder)
+    var tRemainder = diffTime % childSnapshot.val().frequency;
+    console.log(tRemainder);
+    // Minutes away Until Train Arrival
+    var minutesAway = childSnapshot.val().frequency - tRemainder;
+    console.log(minutesAway);
+    // Next Train Arrival
+    var nextTrainArrival = moment().add(minutesAway, "minutes");
+    var nextArrivalConverted = moment(nextTrainArrival).format("HH:mm");
+    console.log(nextArrivalConverted);
+    //Add train times to train schedule
+    var row = $('<tr>');
+    $(row).append($('<td>').text(childSnapshot.val().name));
+    $(row).append($('<td>').text(childSnapshot.val().destination));
+    $(row).append($('<td>').text(childSnapshot.val().frequency));
+    $(row).append($('<td>').text(nextArrivalConverted));
+    $(row).append($('<td>').text(minutesAway));
+    $('#table-body-train-schedule').append(row);
+    // Handles any errors
+}, function(errorObject) {
+    console.log("Errors handled: " + errorObject.code);
+});
